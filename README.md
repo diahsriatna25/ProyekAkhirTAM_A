544b8efa92db0f3b51835d02300e470c727d54f3
diah <diahoppo3737@gmail.com>
2021-12-13 09:22:36 +0700

    Proyek Akhir TAM

---
+++ README.md
@@ -0,0 +1,17 @@
+# RS Bandar Lampung-Android-App
+Sebuah aplikasi android untuk mendapatkan informasi tentang rumah sakit di beberapa kecamatan di kota Bandar Lampung 🇮🇩
+- Data dari API https://github.com/satyawikananda/rs-bed-covid-indo-api/
+# Minimal Versi Android
+Api level 21 (Lollipop)
+# Download Aplikasi
+Kamu bisa mendownloadnya <a href="https://github.com/diahsriatna25/ProyekTAM_A/releases/download/download/base.apk">Disini</a>
+# Tampilan UI
+<img src="https://github.com/diahsriatna25/ProyekTAM_A/tree/download/Screenshoot/mockup1.jpeg"/>
+<img src="https://github.com/diahsriatna25/ProyekTAM_A/tree/download/Screenshoot/mockup2.jpeg"/>
+<img src="https://github.com/diahsriatna25/ProyekTAM_A/tree/download/Screenshoot/mockup3.jpeg"/>
+<img src="https://github.com/diahsriatna25/ProyekTAM_A/tree/download/Screenshoot/mockup4.jpeg"/>
+<img src="https://github.com/diahsriatna25/ProyekTAM_A/tree/download/Screenshoot/mockup5.jpeg"/>
+<img src="https://github.com/diahsriatna25/ProyekTAM_A/tree/download/Screenshoot/mockup6.jpeg"/>
+<img src="https://github.com/diahsriatna25/ProyekTAM_A/tree/download/Screenshoot/mockup7.jpeg"/>
+
+![alt text](https://forthebadge.com/images/badges/built-for-android.svg) ![alt text](https://forthebadge.com/images/badges/built-with-love.svg)
---
+++ app/src/main/java/com/android/rsbandarlampung/model/rumahsakit.java
@@ -0,0 +1,134 @@
+package com.android.rsbandarlampung.model;
+
+public class rumahsakit {
+    String id, nama, alamat, lat, lon, jenis, kelas, tenaga_medis, tenaga_perawat, telp, id_kec, kamarTotal, kamarTersedia, kamarTerpakai;
+
+    public rumahsakit(String id, String nama, String alamat, String lat, String lon, String jenis, String kelas, String tenaga_medis, String tenaga_perawat, String telp, String id_kec, String kamarTotal, String kamarTersedia, String kamarTerpakai) {
+        this.id = id;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.lat = lat;
+        this.lon = lon;
+        this.jenis = jenis;
+        this.kelas = kelas;
+        this.tenaga_medis = tenaga_medis;
+        this.tenaga_perawat = tenaga_perawat;
+        this.telp = telp;
+        this.id_kec = id_kec;
+        this.kamarTotal = kamarTotal;
+        this.kamarTersedia = kamarTersedia;
+        this.kamarTerpakai = kamarTerpakai;
+    }
+
+    public String getKamarTotal() {
+        return kamarTotal;
+    }
+
+    public void setKamarTotal(String kamarTotal) {
+        this.kamarTotal = kamarTotal;
+    }
+
+    public String getKamarTersedia() {
+        return kamarTersedia;
+    }
+
+    public void setKamarTersedia(String kamarTersedia) {
+        this.kamarTersedia = kamarTersedia;
+    }
+
+    public String getKamarTerpakai() {
+        return kamarTerpakai;
+    }
+
+    public void setKamarTerpakai(String kamarTerpakai) {
+        this.kamarTerpakai = kamarTerpakai;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public String getTenaga_medis() {
+        return tenaga_medis;
+    }
+
+    public void setTenaga_medis(String tenaga_medis) {
+        this.tenaga_medis = tenaga_medis;
+    }
+
+    public String getTenaga_perawat() {
+        return tenaga_perawat;
+    }
+
+    public void setTenaga_perawat(String tenaga_perawat) {
+        this.tenaga_perawat = tenaga_perawat;
+    }
+
+    public String getTelp() {
+        return telp;
+    }
+
+    public void setTelp(String telp) {
+        this.telp = telp;
+    }
+
+    public String getId_kec() {
+        return id_kec;
+    }
+
+    public void setId_kec(String id_kec) {
+        this.id_kec = id_kec;
+    }
+}
